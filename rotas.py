727dc15ef3f38d5fb15ebc8102fdff309b56fc68
DanyPaes <danypls.dev@gmail.com>
2023-11-15 18:44:41 -0300

    rotas

--- rotas.py
+++ rotas.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter
-from classes import Presidente, item, gabinete, User
+from classes import Presidente, item, gabinete, User, salvar
 rotas = APIRouter()
 
 
+#inventario(create, read, delete)
 
 @rotas.get('/')
 def home():
-    return 'eu estou funcionando'
+    return 'Pesidentinhos'
+
+
+@rotas.post("/newuser")
+async def criar_usuario(usuario: User):
+    salvar('user', usuario)
+    return usuario
+
