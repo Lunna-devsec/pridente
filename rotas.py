5bce9034eb1192745b8274dc4eaa677f9673aa54
DanyPaes <danypls.dev@gmail.com>
2023-11-16 03:21:30 -0300

    rota criar usuario

--- rotas.py
+++ rotas.py
@@ -1,17 +1,22 @@
-from fastapi import APIRouter
-from classes import Presidente, item, gabinete, User, salvar
+from fastapi import APIRouter, status
+from classes import Presidente, item, gabinete, User, salvar, consultar
 rotas = APIRouter()
 
 
 #inventario(create, read, delete)
 
-@rotas.get('/')
+@rotas.get('/', tags = ["Home"])
 def home():
     return 'Pesidentinhos'
 
+@rotas.get("/inventario",tags=["User"])
+async def curiar(tabela: str, coluna: str, busca: str):
+    teste = consultar(tabela, coluna, busca)
+    return teste
 
-@rotas.post("/newuser")
-async def criar_usuario(usuario: User):
-    salvar('user', usuario.dict())
-    return usuario
+@rotas.post("/newuser", tags=["User"])
+async def criar_usuario(usuario: User)
+    return salvar('user', usuario.dict())
 
+#@rotas.delete("/")
+#async 
