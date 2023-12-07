e3d6d23824d7a0539230b555f979fbfdf67b25af
Luadevsec <lunna.devsec@gmail.com>
2023-12-06 21:36:46 -0300

    o começo de um novo mundo
    eu criarei um mundo novo sem impurezas e sem pessoas ruins usando o jinja2

--- rotas.py
+++ rotas.py
@@ -1,5 +1,18 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, Request
 #from classes import Presidente, item, gabinete, User, salvar, consultar, ver, html
 import classes
 
+
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+
+templates = Jinja2Templates(directory="templates")
+
 rotas = APIRouter()
+
+
+@rotas.get('/teste/{num}', response_class=HTMLResponse)
+def testes(request : Request, num : int):
+    if num == 0:
+        return templates.TemplateResponse('main.html', {"request": request, "mensagem": 'funcionaaaa'})
